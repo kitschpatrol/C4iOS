f2765b59e816f0dcd6479096e004bec6668cf5f4
Travis Kirton <travis@c4ios.com>
2013-10-29 13:53:08 -0700

    Tested use of +(instancetype) in C4Shape subclass.
    This works for C4Shape, so I am going to go ahead and use it everywhere
    else.

---
+++ C4iOS/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  C4iOS
+//
+//  Created by travis on 2013-10-29.
+//  Copyright (c) 2013 Slant. All rights reserved.
+//
+
+#import "C4Shape.h"
+
+@interface Test : C4Shape
+
+@end
---
+++ C4iOS/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  C4iOS
+//
+//  Created by travis on 2013-10-29.
+//  Copyright (c) 2013 Slant. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
