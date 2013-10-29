ac1d9a7486667bc99b2c5a6913bdadf087e51d6f
Travis Kirton <travis@c4ios.com>
2013-10-29 16:09:29 -0700

    Added MIT License to all relevant files.

--- C4iOS/Headers/C4Defines.h
+++ C4iOS/Headers/C4Defines.h
@@ -1,10 +1,21 @@
 //
 //  C4Defines.h
-//  C4iOSDevelopment
+//  C4iOS
 //
-//  Created by Travis Kirton on 11-10-12.
-//  Copyright (c) 2011 mediart. All rights reserved.
+//  Copyright © 2010-2013 Travis Kirton
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+//  and associated documentation files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
 //
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
 
@@ -22,7 +33,10 @@ lineNumber:__LINE__ description:(desc), ##__VA_ARGS__]; \
 } while(0)
 #endif
 
-/* NOT SUPPOSED TO USE #DEFINES, BUT HERE WE DON'T WANT PEOPLE TO CHANGE THE VALUE OF THESE VARIABLES */
+/* 
+ NOT SUPPOSED TO USE #DEFINES,
+ BUT HERE WE DON'T WANT PEOPLE TO CHANGE THE VALUE OF THESE VARIABLES 
+ */
 #ifndef C4_DEFAULT_COLORS
 #define C4RED (UIColor *)[UIColor colorWithRed:1.0f green:0.10f blue:0.10f alpha:1.0f]
 #define C4BLUE (UIColor *)[UIColor colorWithRed:0.043f green:0.627f blue:0.902f alpha:1.0f]
